e24756e0c97038c7dc1e6a375b4e467d2ec04647
Joshua Myers <100216705+Mclovin7291@users.noreply.github.com>
2025-05-01 12:46:16 -0400

    fix files

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:virtualmarketplacefordigitalart/config/firebase_options.dart';
+import 'package:virtualmarketplacefordigitalart/providers/auth_provider.dart';
+import 'package:virtualmarketplacefordigitalart/screens/auth/login_screen.dart';
+import 'package:virtualmarketplacefordigitalart/screens/home/home_screen.dart';
+import 'package:virtualmarketplacefordigitalart/theme/app_theme.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Digital Art Marketplace',
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: ThemeMode.system,
+        debugShowCheckedModeBanner: false,
+        home: const AuthWrapper(),
+      ),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+    
+    return StreamBuilder(
+      stream: authProvider.authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        
+        if (snapshot.hasData) {
+          return const HomeScreen();
+        }
+        
+        return const LoginScreen();
+      },
+    );
+  }
+}
